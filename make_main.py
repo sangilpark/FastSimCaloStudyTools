05e3d9dae545c7e931cdd0df9e1facb8d932803c
Sangil <sangilpark0@gmail.com>
2020-09-22 10:53:32 +0200

    update

--- make_main.py
+++ make_main.py
@@ -4,22 +4,32 @@ import subprocess
 import readline
 import string
 
+path = '/eos/cms/store/group/comm_fastsim/'
 samples = [
-'RezaAnalysis_100GeV',
-'RezaAnalysis_10GeV',
 'RezaAnalysis_1GeV',
-'RezaAnalysis_200GeV',
-'RezaAnalysis_20GeV',
 'RezaAnalysis_2GeV',
-'RezaAnalysis_50GeV',
 'RezaAnalysis_5GeV'
+'RezaAnalysis_7GeV'
+'RezaAnalysis_10GeV',
+'RezaAnalysis_15GeV',
+'RezaAnalysis_20GeV',
+'RezaAnalysis_30GeV',
+'RezaAnalysis_40GeV',
+'RezaAnalysis_50GeV',
+'RezaAnalysis_70GeV',
+'RezaAnalysis_100GeV',
+'RezaAnalysis_150GeV',
+'RezaAnalysis_200GeV',
+'RezaAnalysis_300GeV',
+'RezaAnalysis_400GeV',
+'RezaAnalysis_500GeV',
 ]
 
 
 text = ''
 for num, sample in enumerate(samples):
     text += 'TC = new TChain("g4SimHits/eventTree") ;\n' +\
-    'TC ->Add("' + sample +'*");\n' +\
+    'TC ->Add("'+ path +'/'+ sample +'*");\n' +\
     'GFlash GF' + sample +'(TC);\n' +\
     'GF'+ sample + '.Loop("' + sample + '");\n' +\
     'delete TC;\n' +\
